2180b3531472fd2ea902d73ed167482f899d4ffa
melshafey-vekelz <magedelshafey98@gmail.com>
2023-05-14 21:59:50 +0300

    create statistics section

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
-import React, { useEffect } from "react";
-import AOS from "aos";
-import Nav from "./Component/layout/navbar/Nav";
-import { useTranslation } from "react-i18next";
-import Hero from "./Component/Hero/Hero";
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import Nav from './Component/layout/navbar/Nav';
+import { useTranslation } from 'react-i18next';
+import Hero from './Component/Hero/Hero';
+import Stats from './Component/stats/Stats';
 
 const App = () => {
-  const [t, i18n] = useTranslation();
-  // handle animation
-  useEffect(() => {
-    AOS.init({
-      offset: 0,
-      duration: 1200,
-    });
-  }, []);
-  // handle page direction
-  useEffect(() => {
-    document.documentElement.setAttribute("lang", i18n.language);
-    if (i18n.language === "ar") {
-      document.getElementsByTagName("body")[0].style.direction = "rtl";
-    } else {
-      document.getElementsByTagName("body")[0].style.direction = "ltr";
-    }
-  }, [i18n.language]);
-  return (
-    <div>
-      <Nav />
-      <Hero />
-    </div>
-  );
+	const [t, i18n] = useTranslation();
+	// handle animation
+	useEffect(() => {
+		AOS.init({
+			offset: 0,
+			duration: 1200,
+		});
+	}, []);
+	// handle page direction
+	useEffect(() => {
+		document.documentElement.setAttribute('lang', i18n.language);
+		if (i18n.language === 'ar') {
+			document.getElementsByTagName('body')[0].style.direction = 'rtl';
+		} else {
+			document.getElementsByTagName('body')[0].style.direction = 'ltr';
+		}
+	}, [i18n.language]);
+	return (
+		<div>
+			<Nav />
+			<Hero />
+			<Stats />
+		</div>
+	);
 };
 
 export default App;
--- src/fakers/data.js
+++ src/fakers/data.js
@@ -1,45 +1,65 @@
 // social media
-import face from "../assets/social/icons8-facebook-48.png";
-import whats from "../assets/social/icons8-whatsapp-48.png";
-import insta from "../assets/social/icons8-instagram-48.png";
-import behance from "../assets/social/behance.png";
+import face from '../assets/social/icons8-facebook-48.png';
+import whats from '../assets/social/icons8-whatsapp-48.png';
+import insta from '../assets/social/icons8-instagram-48.png';
+import behance from '../assets/social/behance.png';
 export const navLinks = [
-  {
-    title: "Home",
-    ArabicTitle: "الرئيسية",
-    path: "/",
-  },
-  {
-    title: "About Us",
-    ArabicTitle: "من نحن",
-    path: "#about",
-  },
-  {
-    title: "Services",
-    ArabicTitle: "خدماتنا",
-    path: "#services",
-  },
-  {
-    title: "Behance",
-    ArabicTitle: "بيهانس",
-    path: "https://www.google.com",
-  },
+	{
+		title: 'Home',
+		ArabicTitle: 'الرئيسية',
+		path: '/',
+	},
+	{
+		title: 'About Us',
+		ArabicTitle: 'من نحن',
+		path: '#about',
+	},
+	{
+		title: 'Services',
+		ArabicTitle: 'خدماتنا',
+		path: '#services',
+	},
+	{
+		title: 'Behance',
+		ArabicTitle: 'بيهانس',
+		path: 'https://www.google.com',
+	},
 ];
 export const socialMedia = [
-  {
-    icon: face,
-    path: "https://www.google.com",
-  },
-  {
-    icon: whats,
-    path: "https://wa.me/+201022153359",
-  },
-  {
-    icon: insta,
-    path: "https://www.google.com",
-  },
-  {
-    icon: behance,
-    path: "https://www.google.com",
-  },
+	{
+		icon: face,
+		path: 'https://www.google.com',
+	},
+	{
+		icon: whats,
+		path: 'https://wa.me/+201022153359',
+	},
+	{
+		icon: insta,
+		path: 'https://www.google.com',
+	},
+	{
+		icon: behance,
+		path: 'https://www.google.com',
+	},
+];
+export const stats = [
+	{
+		num: '+15',
+		title: 'Years of Experience',
+		ArabicTitle: 'عام من الخبرة',
+		delay: '400',
+	},
+	{
+		num: '+150',
+		title: 'E-Commerce',
+		ArabicTitle: 'متجر الكتروني',
+		delay: '600',
+	},
+	{
+		num: '+1435',
+		title: 'partner',
+		ArabicTitle: 'شريك نجاح',
+		delay: '800',
+	},
 ];
